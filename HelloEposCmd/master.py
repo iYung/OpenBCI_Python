a459b1e3820bc6eae1fd55207ba77b15a607c7cc
pwnedpixel <s1l3nt.n00bz@gmail.com>
2019-03-30 18:30:08 -0400

    add HelloEposCmd

---
+++ HelloEposCmd/master.py
@@ -0,0 +1,17 @@
+import os
+#import initMotor
+#import haltMotor
+#import subprocess
+
+#from subprocess import call
+#call('echo "test"', shell=True)
+#call('echo "./initMotor"', shell=True)
+#call('echo "./haltMotor"', shell=True)
+#echo = "echo"
+#os.system(echo +" "+ "./initMotor")
+#os.system(echo +" ./haltMotor")
+#os.system("echo startMotor.py")
+#os.system("echo haltMotor.py")
+
+os.system("/home/pi/Downloads/EPOS_Linux_Library/examples/HelloEposCmd/initMotor")
+os.system("/home/pi/Downloads/EPOS_Linux_Library/examples/HelloEposCmd/haltMotor")
