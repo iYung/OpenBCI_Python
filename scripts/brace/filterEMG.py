77142a34cf1e0a45d679369fa352ef7cb2c671eb
Andy K <andy@akeech.com>
2019-04-02 22:44:39 -0400

    added semi notch filter

--- scripts/brace/filterEMG.py
+++ scripts/brace/filterEMG.py
@@ -14,11 +14,24 @@ def filterEMG(time, emg, sfreq=800, low_band=50, high_band=15, low_pass=10):
     high_band = high_band/(1.0*sfreq/2)
     low_band = low_band/(1.0*sfreq/2)
     b, a = sp.signal.butter(4, [high_band, low_band], btype='bandpass')
-    b1, a1 = signal.iirnotch(60, 30, sfreq)
+    emg_bandpassed = sp.signal.filtfilt(b, a, emg)
+
+    #simulating a notch filter
+    #this is the lower freq part of it
+    high_band1 = 0/(1.0*sfreq/2)
+    low_band1 = 58/(1.0*sfreq/2)
+    b1, a1 = sp.signal.butter(5, [high_band1, low_band1], btype='bandpass')
+    #this is the higher freq part of it
+    high_band2 = 62/(1.0*sfreq/2)
+    low_band2 = sfreq/(1.0*sfreq/2)
+    b1, a1 = sp.signal.butter(5, [high_band2, low_band2], btype='bandpass')
+
+    # b1, a1 = signal.iirnotch(60, 30, sfreq)
     # normalize the signal
     #emg_norm = [i / 500.0 for i in emg]
 
-    emg_filtered = sp.signal.filtfilt(b, a, emg)
-    emg_filtered2 = sp.signal.filtfilt(b1, a1, emg_filtered)
+    emg_filtered = sp.signal.filtfilt(b, a, emg_bandpassed) + sp.signal.filtfilt(b, a, emg_bandpassed)
+    # emg_bandpass = sp.signal.filtfilt(b, a, emg)
+    # emg_notch_low = sp.signal.filtfilt(b1, a1, emg_bandpass)
     # return emg_filtered
-    return emg_filtered2
+    return emg_filtered
